9929c721ff080a55b2ca0c683594f6a8cee8f129
Riley Carlson <riley.carlson@ksb.com>
2023-01-11 14:43:54 -0500

    Add SafeMaxBy

--- GroupMeAPI/Utility.cs
+++ GroupMeAPI/Utility.cs
@@ -17,12 +17,23 @@ namespace GroupMeAPI
 		}
 		/// <summary> Converts the date component of a <see cref="DateTime"/> object to <see cref="string"/> </summary>
 		public static string DateToString(DateTime dateTime) => $"{dateTime.Month}/{dateTime.Day}/{dateTime.Year}";
-		public static TResult? SafeMax<TSource, TResult>(IEnumerable<TSource> source, Func<TSource, TResult> selector, TResult defaultValue = default)
+		
+		public static TSource? SafeMaxBy<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, TResult> selector, TSource? defaultValue = default)
+		{
+			if (source is null || !source.Any()) return defaultValue;
+			else return source.MaxBy(selector);
+		}
+		public static TSource? SafeMinBy<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, TResult> selector, TSource? defaultValue = default)
+		{
+			if (source is null || !source.Any()) return defaultValue;
+			else return source.MinBy(selector);
+		}
+		public static TResult? SafeMax<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, TResult> selector, TResult? defaultValue = default)
 		{
 			if (!source.Any()) return defaultValue;
 			else return source.Max(selector);
 		}
-		public static TResult SafeMin<TSource, TResult>(IEnumerable<TSource> source, Func<TSource, TResult> selector, TResult defaultValue = default)
+		public static TResult? SafeMin<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, TResult> selector, TResult? defaultValue = default)
 		{
 			if (!source.Any()) return defaultValue;
 			else return source.Min(selector);
